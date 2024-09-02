e1a55808cf0f759d8988fcf1cad538b2a8e10f48
Gustav Polhem <gustavpolhem@gmail.com>
2024-09-02 21:18:12 +0200

    added setup

--- MVC.Budget/Program.cs
+++ MVC.Budget/Program.cs
@@ -1,27 +1,14 @@
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+using MVC.Budget;
+
+var app = Setup.CreateWebApplication();
+
+if(app is null)
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    Console.WriteLine("App could not start. Exiting");
+    Environment.Exit(1);
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
